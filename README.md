491a8ce48f336dc965c04c8b0541da0ab46aabc4
Eduardo Garro <efgarro@yahoo.com>
2025-08-17 13:06:35 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,66 @@
+
+```
+BijaLapa Natural	
+
+   View Rooms
+    Guarumo
+    Sunrise
+
+   The Grounds	
+    The Yard
+    Meet Organic
+    Rural Work
+
+   Activities	
+
+	Local Restaurants
+	Hiking
+	Fogata Night
+	Visit La Poza
+	Visit Bijagual Waterfall
+	The Town of Bijagual and Sorroundings
+	Horseback Tours
+	Crocodile Tours (One-hour drive or less)
+		
+   Scarlet Macaw Habitat Project
+
+	Wildlife Journal
+	One Hundred Trees a Year (and Counting)
+
+   Local Partners
+
+   Contact
+
+   About Us
+
+Routes: 
+
+/ <show photoStory>
+/view-rooms <show photoStory>
+/view-rooms/guarumo 
+/view-rooms/sunrise
+
+/grounds <show photoStory>
+/grounds/yard
+/groungs/organic
+/grounds/work
+
+/activities <show photoStory>
+/activities/restaurants
+/activities/hiking
+/activities/fogata-night
+/activities/la-poza
+/activities/waterfall
+/activities/horseback
+/activities/bijagual
+/activities/croc-tour
+
+/sm-habitat <show photoStory>
+/sm-habitat/wildlife
+/sm-habitat/trees
+
+/local-partners
+/contact
+/about-us
+/terms-and-conditions
+```
---
+++ src/routes/__root.tsx
@@ -0,0 +1,62 @@
+// src/routes/__root.tsx
+/// <reference types="vite/client" />
+import type { ReactNode } from "react";
+import {
+  Outlet,
+  HeadContent,
+  Scripts,
+  createRootRouteWithContext,
+} from "@tanstack/react-router";
+
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import { QueryClient } from "@tanstack/react-query";
+
+
+import appStyles from "../css/styles.css?url";
+import appCss from "../styles/app.css?url";
+
+export const Route = createRootRouteWithContext<{
+  queryClient: QueryClient;
+}>()({
+  head: () => ({
+    meta: [
+      {
+        charSet: "utf-8",
+      },
+      {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1",
+      },
+      {
+        title: "113-Start-Twind-CssM-Query",
+      },
+    ],
+    links: [{ rel: "stylesheet", href: appCss }, { rel: "stylesheet", href: appStyles }],
+  }),
+  component: RootComponent,
+});
+
+function RootComponent() {
+  return (
+    <RootDocument>
+      <Outlet />
+    </RootDocument>
+  );
+}
+
+function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
+  return (
+    <html>
+      <head>
+        <HeadContent />
+      </head>
+      <body>
+        {children}
+        <TanStackRouterDevtools position="bottom-right" />
+        <ReactQueryDevtools buttonPosition="bottom-left" />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
